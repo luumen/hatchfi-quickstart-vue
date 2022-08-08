8b76a896c5b84efef5ce1c9373b27f1a42e4ace3
Casey Wilcox <caseywilcox@outlook.com>
2022-08-07 21:08:26 -0700

    pushing changes

--- src/components/ConnectHatchfi.vue
+++ src/components/ConnectHatchfi.vue
@@ -1,12 +1,28 @@
 <script setup>
 import axios from "axios";
-import { ref, onMounted, onBeforeUnmount } from "vue";
-import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
-import { CheckIcon } from '@heroicons/vue/outline'
+import { ref, onMounted, onBeforeUnmount, watch } from "vue";
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot, Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
+import { CheckIcon, ChevronDownIcon } from '@heroicons/vue/outline'
 
 const emit = defineEmits(['passid']);
 
+<<<<<<< Updated upstream
 let userId = "<INPUT USERNAME HERE>"; // In this case, we'll just declare our users ID here.
+=======
+const publishingOptions = [
+  { title: 'Ethereum', description: '0xd853f83605f7199CE2b9B61807b4084a3909F783', current: true },
+  { title: 'Polygon', description: '0xA4FcE871c814a193Fb855330510B4C493a5F30cB', current: false },
+  { title: 'Bitcoin', description: '1Lj6T9wt3nCVo1cuzn94rU5uH3M319FP5', current: false },
+]
+
+let copied = ref(publishingOptions[0])
+
+const copyText = (text) => {
+  navigator.clipboard.writeText(text);
+}
+
+let userId = "hatchfi-quickstart-user-1"; // In this case, we'll just declare our users ID here.
+>>>>>>> Stashed changes
 let showFrame = ref(false);
 // let linkUrl = ref("https://link.hatchfi.co/?clientId=<INPUT_CLIENTID_HERE>&token=");
 let linkUrl = ref("http://localhost:3000/?clientId=<INSERT_CLIENT_ID_HERE>&token=");
@@ -19,32 +35,22 @@ onBeforeUnmount(() => {
 })
 
 const generateLink = async () => {
+  let userData = await axios.post("http://localhost:3003/auth-user", { userId: userId })
 
-  // In this case, we'll look for a sessionToken in our localStorage
-  // to see if we need to "login" our user.
-  if (!localStorage.getItem("hatchfiSessionToken")) {
-    // Lets authenticate our user
-    // let userData = await axios.post("http://localhost:3003/auth-user", { userId: userId })
-    let userData = await axios.post("http://localhost:3003/auth-user", { userId: userId })
+  sessionToken.value = userData.data.token;
+  showFrame.value = true;
 
-
-    console.log(userData.data)
-    sessionToken.value = userData.data.token;
-    showFrame.value = true;
-
-    window.addEventListener("message", (event) => {
-      if (event.data.hatchfi) {
-        console.log(event.data.result.data)
-        if (event.data.result.data.status == "success") {
-          console.log("Pass id to app", event.data.result.data.accountId)
-          emit('passid', event.data.result.data.accountId);
-        }
+  window.addEventListener("message", (event) => {
+    if (event.data.hatchfi) {
+      console.log(event.data)
+      if (event.data.result.status == "success") {
+        console.log("Pass id to app", event.data.result.data.accountId)
+        emit('passid', event.data.result.data.accountId);
+      } else if (event.data.result.status == "close") {
+        showFrame.value = false;
       }
-    });
-
-    // localStorage.setItem("hatchfiSessionToken", userData.data.token);
-  }
-
+    }
+  });
 }
 
 </script>
@@ -55,13 +61,44 @@ const generateLink = async () => {
       <span class="block">Ready to dive in?</span>
       <span class="block">Let's connect an account</span>
     </h2>
-    <div class="mt-8 flex justify-center">
-      <div class="inline-flex rounded-md shadow">
-        <button @click="generateLink"
-          class="inline-flex duration-200 items-center justify-center px-10 py-3 border border-transparent text-base font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700">
-          Connect a crypto account</button>
+    <Listbox as="div" v-model="copied" class="mt-8">
+      <ListboxLabel class="sr-only"> Connect a crypto account </ListboxLabel>
+      <div class="relative">
+        <div class="inline-flex shadow-sm rounded-md divide-x divide-emerald-600">
+          <div class="relative z-0 inline-flex shadow-sm rounded-md divide-x divide-emerald-700">
+            <button @click="generateLink"
+              class="relative inline-flex items-center bg-emerald-600 py-3 pl-3 pr-4 border border-transparent rounded-l-md shadow-sm text-white outline-none hover:bg-emerald-700">
+              <p class="ml-2.5 text-sm font-medium">Connect a crypto account</p>
+            </button>
+            <ListboxButton
+              class="relative inline-flex items-center bg-emerald-600 px-3 p-2 rounded-l-none rounded-r-md font-medium text-white outline-none hover:bg-emerald-700">
+              <ChevronDownIcon class="h-5 w-5 text-white" aria-hidden="true" />
+            </ListboxButton>
+          </div>
+        </div>
+
+        <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+          <ListboxOptions
+            class="origin-top-right absolute z-10 right-0 mt-2 w-72 rounded-md shadow-lg overflow-hidden bg-white divide-gray-200 ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <ListboxOption as="template" v-for="option in publishingOptions" :key="option.title" :value="option" v-slot="{ active, selected }">
+              <li @click="copyText(option.description)"
+                :class="[active ? 'text-white bg-emerald-600' : 'text-gray-900', 'cursor-pointer select-none relative p-3 text-sm']">
+                <div class="flex flex-col">
+                  <div class="flex justify-between">
+                    <p :class="selected ? 'font-semibold' : 'font-normal'">
+                      {{ option.title }}
+                    </p>
+                  </div>
+                  <p :class="[active ? 'text-emerald-200' : 'text-gray-500', 'mt-1 text-left']">
+                    {{ option.description.substring(0, 14) }}...{{ option.description.substring(option.description.length - 14) }}
+                  </p>
+                </div>
+              </li>
+            </ListboxOption>
+          </ListboxOptions>
+        </transition>
       </div>
-    </div>
+    </Listbox>
   </div>
 
   <TransitionRoot as="template" :show="showFrame">
@@ -71,14 +108,14 @@ const generateLink = async () => {
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
       </TransitionChild>
 
-      <div class="fixed z-10 inset-0 overflow-y-auto">
+      <div class="fixed z-10 inset-0 overflow-y-auto" v-if="showFrame">
         <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100"
             leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
 
-            <iframe class="rounded-2xl" v-if="showFrame" :src="linkUrl + sessionToken" name="hatchfi_link" height="608px" width="360px"
-              allow="solana; ethereum;" title="Hatchfi Link"></iframe>
+            <iframe class="rounded-2xl" :src="linkUrl + sessionToken" name="hatchfi_link" height="608px" width="360px" allow="solana; ethereum;"
+              title="Hatchfi Link"></iframe>
 
           </TransitionChild>
         </div>
